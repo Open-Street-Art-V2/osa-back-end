040b2bc9df242d0213d9a9012f55b2c32034f784
Ahmadou kassoum <ahmadoukassoum7@gmail.com>
2022-04-12 21:48:13 +0200

    fix bug if anonymous user try to get contribution

--- src/proposition/contribution/contribution.controller.ts
+++ src/proposition/contribution/contribution.controller.ts
@@ -31,7 +31,6 @@ export class ContributionController {
   constructor(private readonly contributionService: ContributionService) {}
 
   @Get()
-  @JwtAuth(Role.ADMIN, Role.USER)
   async getContribution(@Query() paginationDto: PaginationDto) {
     if (Object.keys(paginationDto).length === 2) {
       return this.contributionService.findAllContribution({
