287d881cd293da20f15c5ea70a4e54f8cc34e68e
Yacine <bouyakoubyacine1@gmail.com>
2022-04-14 11:03:27 +0200

    fix module

--- src/trophie/trophie.module.ts
+++ src/trophie/trophie.module.ts
@@ -3,9 +3,11 @@ import { TrophieService } from './trophie.service';
 import { TrophieController } from './trophie.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
+import { Trophie } from './entities/trophie.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Trophie]), UsersModule],
   controllers: [TrophieController],
   providers: [TrophieService],
 })
