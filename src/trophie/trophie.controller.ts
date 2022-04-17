829812dde1e6bbb3f6bbe4c010ab74e2380c8e05
manon <manon.guillard@univ-rouen.fr>
2022-04-17 20:43:13 +0200

    fix bug if visitor try to get trophies

--- src/trophie/trophie.controller.ts
+++ src/trophie/trophie.controller.ts
@@ -11,20 +11,16 @@ import {
 import { TrophieService } from './trophie.service';
 import { CreateTrophieDto } from './dto/create-trophie.dto';
 import { UpdateTrophieDto } from './dto/update-trophie.dto';
-import { JwtAuth } from 'src/auth/decorators/auth.decorator';
-import { Role } from 'src/auth/roles/role.enum';
 // import { UsersService } from 'src/users/users.service';
 import { PaginationDto } from 'src/proposition/dto/pagination.dto';
 
 @Controller('trophie')
 export class TrophieController {
   constructor(
-    private readonly trophieService: TrophieService,
-  ) // private readonly userService: UsersService,
-  {}
+    private readonly trophieService: TrophieService, // private readonly userService: UsersService,
+  ) {}
 
   @Get('paginate/:id')
-  @JwtAuth(Role.ADMIN, Role.USER)
   async getTrophiesPaginate(
     @Query() paginationDto: PaginationDto,
     @Param('id') userId,
